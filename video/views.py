90393b4b9fe866f673b37e6a76d5a3cdb661082d
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-05-12 21:07:10 +0200

    Create templates, render and some css to it

--- video/views.py
+++ video/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+videos = [
+    {
+        'title': 'Devs',
+        'genre': 'Sci-Fi',
+        'duration': 50,
+        'release_dates': '2020.01.01'
+    },
+    {
+        'title': 'WestWorld',
+        'genre': 'Sci-Fi',
+        'duration': 45,
+        'release_dates': '2018.02.01'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Video Rental Home</h1>')
+    context = {
+        'videos': videos
+    }
+    return render(request, 'video/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'video/about.html')
