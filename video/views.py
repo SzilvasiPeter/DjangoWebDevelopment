5d530fbbf5ac4e6dfea47be554a8f0d01adc3a72
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-05-15 16:04:29 +0200

    Added new user into Django admin site, and organize view

--- video/views.py
+++ video/views.py
@@ -16,12 +16,22 @@ videos = [
     }
 ]
 
+rented_videos = [
+    {
+        'title': 'Breaking Bad',
+        'genre': 'Action',
+        'duration': 35,
+        'release_dates': '2015.06.12'
+    },
+]
+
 
 def home(request):
     context = {
-        'videos': videos
+        'videos': videos,
+        'rented_videos': rented_videos
     }
-    return render(request, 'video/home.html', context)
+    return render(request, 'video/home.html', context=context)
 
 
 def about(request):
