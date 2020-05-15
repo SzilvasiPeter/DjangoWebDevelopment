fa88184399f34187600f60bc4b0678d52ce503b2
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-05-15 17:29:51 +0200

    Using database for video, Video model was created

--- video/models.py
+++ video/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=30)
+    duration = models.IntegerField()
+    receive_date = models.DateTimeField(auto_now_add=True)
+    isRented = models.BooleanField(default=False)
+
+    # TODO: when deleting user set the video isRented field to False
+    renter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+    # 'title': 'Devs',
+    # 'genre': 'Sci-Fi',
+    # 'duration': 50,
+    # 'release_dates': '2020.01.01'
--- video/views.py
+++ video/views.py
@@ -1,35 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-videos = [
-    {
-        'title': 'Devs',
-        'genre': 'Sci-Fi',
-        'duration': 50,
-        'release_dates': '2020.01.01'
-    },
-    {
-        'title': 'WestWorld',
-        'genre': 'Sci-Fi',
-        'duration': 45,
-        'release_dates': '2018.02.01'
-    }
-]
-
-rented_videos = [
-    {
-        'title': 'Breaking Bad',
-        'genre': 'Action',
-        'duration': 35,
-        'release_dates': '2015.06.12'
-    },
-]
+from .models import Video
 
 
 def home(request):
     context = {
-        'videos': videos,
-        'rented_videos': rented_videos
+        'videos': Video.objects.all()
     }
     return render(request, 'video/home.html', context=context)
 
